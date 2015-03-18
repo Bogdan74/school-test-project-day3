e03c72864a13834ec07f8cd43075e619810cbfc5
Bogdan74 <vinnicareserve@gmail.com>
2015-03-19 01:02:24 +0200

    usersController.js

---
+++ controllers/usersController.js
@@ -0,0 +1,59 @@
+//usersController
+var users = require('../lib/Users');
+var http = require('http');
+var url = require('url');
+var qs = require('querystring');
+
+var app = http.createServer().listen(8080);
+
+app.on('request', function(request, response){
+		
+	if (request.method == 'POST') {
+        var body = '';
+        request.on('data', function (data) {
+            body += data;
+
+            if (body.length > 1e6)
+                request.connection.destroy();
+        });
+        request.on('end', function () {
+            var post = qs.parse(body);
+			
+			//var tmpObj = JSON.parse(post);
+			console.log(body);
+			console.log(post);
+			
+			//post должна содержать строку-валидную для JSON.parse()
+			// у меня не получилось получить строку, которая бы нормально парсилась
+			// для проверки роботоспособности посылаю временный объект tmpObj
+			var tmpObj = {'name':'John', 'e-mail': 'john@smith.com', 'description': 'admin', 'age': 25};
+			
+			users.addUser(tmpObj);
+        });
+    }
+	
+});
+
+app.on('request', function(request, response){
+	var path = url.parse(request.url).pathname;
+	
+	switch(path){
+		case '/':
+			response.writeHead(200, {'Content-Type': 'text/html;charset=utf-8'});
+			response.write('Ви на головній сторінці.');
+			break;
+		case '/users':
+			response.writeHead(200, {'Content-Type': 'application/json'});
+			response.write(JSON.stringify(users.getUsers()).toString());
+			break;
+		default:
+			response.writeHead(404);
+			response.write('404: Page not found!');
+		
+	}
+	response.end();
+});
+
+app.on('listening', function(){
+	console.log('Listening on 8080...');
+});
